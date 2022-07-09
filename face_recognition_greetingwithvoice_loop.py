78f4d97872c2f3fcbeace495f10af1936c487117
Alsafa Neha <alsafaneha1755@gmail.com>
2022-07-09 11:34:29 +0530

    Updating

---
+++ face_recognition_greetingwithvoice_loop.py
@@ -0,0 +1,103 @@
+import face_recognition
+import cv2
+import numpy as np
+import os
+import datetime
+import pyttsx3 as pyttsx
+import time
+
+currentTime = datetime.datetime.now()
+currentTime.hour
+
+engine = pyttsx.init()
+rate = engine.getProperty('rate')
+engine.setProperty('rate', rate-40)
+
+
+video_capture = cv2.VideoCapture(0)
+
+image1 = face_recognition.load_image_file(os.path.abspath("Sir1.jpeg"))
+image1_face_encoding = face_recognition.face_encodings(image1)[0]
+
+image2 = face_recognition.load_image_file(os.path.abspath("Sir2.jpeg"))
+image2_face_encoding = face_recognition.face_encodings(image2)[0]
+
+image3 = face_recognition.load_image_file(os.path.abspath("me.jpg"))
+image3_face_encoding = face_recognition.face_encodings(image3)[0]
+
+known_face_encodings = [ 
+    image1_face_encoding,
+    image2_face_encoding,
+    image3_face_encoding
+]
+
+known_face_names = [ 
+    "Sudarshan",
+    "Vishal",
+    "Alsafa"
+]
+
+face_locations = []
+face_encodings = []
+face_names = []
+process_this_frame = True
+
+while True:
+    ret, frame = video_capture.read()
+    small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+    rgb_small_frame = small_frame[:, :, ::-1]
+    print(rgb_small_frame)
+    if process_this_frame:
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+
+
+        face_names = []
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance=0.6)
+            name = "Unkonwn"
+            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+            best_match_index = np.argmin(face_distances)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+                if 5<= currentTime.hour < 12:
+                     result = ('Good Morning ' + name)
+                elif 12 <= currentTime.hour < 18:
+                     result = ('Good Afternoon ' + name)
+                else:
+                     result = ('Good Evening ' + name)
+                engine.say(result)
+                engine.say('Nice to meet you')
+                time.sleep(0.3)
+
+            face_names.append(name)
+
+    
+    process_this_frame = not process_this_frame
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
+        top *= 4
+        right *= 4
+        bottom *= 4
+        left *= 4
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.6, (255, 255, 255), 1)
+
+
+
+    engine.runAndWait()
+    
+
+
+    cv2.imshow('Video', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+    
+
+video_capture.release()
+cv2.destroyAllWindows() 
+
+exit()
