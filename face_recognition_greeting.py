e4869b541db1fede0dc0d7c9f08cf24817884bce
Alsafa Neha <alsafaneha1755@gmail.com>
2022-07-09 10:15:04 +0530

    Updated version

--- face_recognition_greeting.py
+++ face_recognition_greeting.py
@@ -4,8 +4,8 @@ import numpy as np
 import os
 import datetime
 
-
-hour = datetime.datetime.now().hour
+currentTime = datetime.datetime.now()
+currentTime.hour
 
 video_capture = cv2.VideoCapture(0)
 
@@ -18,12 +18,13 @@ image2_face_encoding = face_recognition.face_encodings(image2)[0]
 image3 = face_recognition.load_image_file(os.path.abspath("me.jpg"))
 image3_face_encoding = face_recognition.face_encodings(image3)[0]
 
-known_face_encodings = [
+known_face_encodings = [ 
     image1_face_encoding,
     image2_face_encoding,
     image3_face_encoding
 ]
-known_face_names = [
+
+known_face_names = [ 
     "Sudarshan",
     "Vishal",
     "Alsafa"
@@ -32,7 +33,6 @@ known_face_names = [
 face_locations = []
 face_encodings = []
 face_names = []
-face_greetings = []
 process_this_frame = True
 
 while True:
@@ -44,29 +44,26 @@ while True:
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
+
         face_names = []
         for face_encoding in face_encodings:
             matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance=0.6)
-            name = "Unknown" 
+            name = "Unkonwn"
             face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = known_face_names[best_match_index]
+                if 5<= currentTime.hour < 12:
+                     print('Good Morning ' + name)
+                elif 12 <= currentTime.hour < 18:
+                     print('Good Afternoon ' + name)
+                else:
+                     print('Good Evening ' + name)
             face_names.append(name)
 
-        face_greetings = []
-        for face_encoding in face_encoding:
-            matches = face_recognition.compare_faces(known_face_encodings, face_encoding, tolerance=0.6)
-            greeting = "Good morning" if 5<=hour<12 else "Good afternoon" if hour<18 else "Good evening"
-            face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                greeting = "Good morning" if 5<=hour<12 else "Good afternoon" if hour<18 else "Good evening"
-            face_greetings.append(greeting)
-        
+    
     process_this_frame = not process_this_frame
-    print ("Face detected -- {}".format(face_names, face_greetings))
-    for (top, right, bottom, left), name in zip(face_locations, face_names), greeting in zip(face_locations, face_greetings):
+    for (top, right, bottom, left), name in zip(face_locations, face_names):
         top *= 4
         right *= 4
         bottom *= 4
@@ -74,7 +71,8 @@ while True:
         cv2.rectangle(frame, (left, top), (right, bottom), (0, 255, 0), 2)
         cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 255, 0), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
-        cv2.putText(frame, name, greeting, (left + 6, bottom - 6), font, 0.6, (255, 255, 255), 1)
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.6, (255, 255, 255), 1)
+
 
     cv2.imshow('Video', frame)
 
@@ -82,4 +80,4 @@ while True:
         break
 
 video_capture.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() 
--- face_recognition_greetingwithvoice.py
+++ face_recognition_greetingwithvoice.py
@@ -2,6 +2,13 @@ import face_recognition
 import cv2
 import numpy as np
 import os
+import datetime
+from gtts import gTTS
+from playsound import playsound
+
+
+currentTime = datetime.datetime.now()
+currentTime.hour
 
 video_capture = cv2.VideoCapture(0)
 
@@ -14,12 +21,13 @@ image2_face_encoding = face_recognition.face_encodings(image2)[0]
 image3 = face_recognition.load_image_file(os.path.abspath("me.jpg"))
 image3_face_encoding = face_recognition.face_encodings(image3)[0]
 
-known_face_encodings = [
+known_face_encodings = [ 
     image1_face_encoding,
     image2_face_encoding,
     image3_face_encoding
 ]
-known_face_names = [
+
+known_face_names = [ 
     "Sudarshan",
     "Vishal",
     "Alsafa"
@@ -38,6 +46,7 @@ while True:
     if process_this_frame:
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+    
 
         face_names = []
         for face_encoding in face_encodings:
@@ -47,10 +56,23 @@ while True:
             best_match_index = np.argmin(face_distances)
             if matches[best_match_index]:
                 name = known_face_names[best_match_index]
+            if 5 <= currentTime.hour < 12:
+                 result = ('Good Morning ' + name)
+            elif 12 <= currentTime.hour < 18:
+                 result = ('Good Afternoon ' + name)
+            else:
+                 result = ('Good Evening ' + name)
+            print(result)
+            language = 'en'
+            output = gTTS(text=result, lang=language, slow=False)
+            output.save("output.mp3")
+            os.system("start output.mp3")
+            playsound("start output.mp3")
+
             face_names.append(name)
 
+    
     process_this_frame = not process_this_frame
-    print ("Face detected -- {}".format(face_names))
     for (top, right, bottom, left), name in zip(face_locations, face_names):
         top *= 4
         right *= 4
@@ -62,9 +84,11 @@ while True:
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 0.6, (255, 255, 255), 1)
 
     cv2.imshow('Video', frame)
+    
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+
 video_capture.release()
-cv2.destroyAllWindows()
+cv2.destroyAllWindows() 
